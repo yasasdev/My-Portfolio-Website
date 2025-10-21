5d4a87c7ebcb4619b723600cd6434c17bb8a9130
yasasdev <yasaslekamge@gmail.com>
2025-10-21 15:54:54 +0530

    update About component styles

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -4,8 +4,8 @@ import React from "react";
 
 const About = () => {
   return (
-    <div id="about" className="w-full px-[12%] py-10 scroll-mt-20">
-      <h4 className="text-center mb-2 text-lg font-Ovo">Introduction</h4>
+    <div id="about" className="w-full px-[12%] py-20 scroll-mt-20">
+      <h4 className="text-center mb-2 text-lg font-Ovo mt-10">Introduction</h4>
       <h2 className="text-center text-4xl font-Ovo">About Me</h2>
       <div className="flex w-full flex-col lg:flex-row items-center gap-20 my-20">
         <div className="w-64 sm:w-80 rounded-3xl max-w-none">
@@ -16,7 +16,7 @@ const About = () => {
           />
         </div>
         <div className="flex-1">
-          <p className="mb-10 max-w-2xl font-Ovo">
+          <p className="mb-10 max-w-2xl font-Ovo -mt-10">
             Software Engineer with a Higher National Diploma and a Diploma in
             Software Engineering from NIBM. Currently enhancing my expertise
             through an internship at Innova Global Lanka, contributing to
---
+++ app/components/Services.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Services = () => {
+  return (
+    <div id='services' className="w-full px-[12%] py-10 scroll-mt-20">
+      <h4 className="text-center mb-2 text-lg font-Ovo">What I offer</h4>
+      <h2 className="text-center text-4xl font-Ovo">My Services</h2>
+    </div>
+  )
+}
+
+export default Services
