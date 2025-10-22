78d4b4693668416d98fe18be0073195a7d002517
yasasdev <yasaslekamge@gmail.com>
2025-10-22 12:05:35 +0530

    Add Footer component with responsive design and contact information

---
+++ app/components/Footer.jsx
@@ -0,0 +1,43 @@
+import { assets } from "@/assets/assets";
+import Image from "next/image";
+import React from "react";
+
+const Footer = () => {
+  const year = new Date().getFullYear();
+
+  return (
+    <div className="mt-20">
+      <div className="text-center">
+        <Image src={assets.logo} alt="Logo" className="w-28 mx-auto mb-2" />
+        <div className="w-max flex items-center gap-2 mx-auto">
+          <Image
+            src={assets.mail_icon}
+            alt="mail icon"
+            className="w-5 mx-auto mb-2"
+          />
+          yasaslekamge@gmail.com
+        </div>
+      </div>
+      <div
+        className="text-center sm:flex items-center justify-between 
+        border-t border-gray-400 mx-[10%] mt-6 py-2"
+      >
+        <p className="text-gray-500">© {year} Yasas Lekamge. All rights reserved.</p>
+        <ul className="flex items-center text-gray-500 gap-10 justify-center mt-4 sm:mt-0">
+          <li>
+            <a target="_blank" href="https://www.github.com/yasasdev">
+              GitHub
+            </a>
+          </li>
+          <li>
+            <a target="_blank" href="https://www.linkedin.com/in/yasasdev">
+              LinkedIn
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- app/page.js
+++ app/page.js
@@ -6,6 +6,7 @@ import About from './components/About';
 import Services from './components/Services';
 import Work from './components/Work';
 import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 export default function Home() {
   return (
@@ -16,6 +17,7 @@ export default function Home() {
       <Services />
       <Work />
       <Contact />
+      <Footer />
     </>
   );
 }
