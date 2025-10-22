4995b765a77c05af05c36463e0c577620043bce6
yasasdev <yasaslekamge@gmail.com>
2025-10-22 10:50:36 +0530

    Add Contact component with form submission functionality and styling

---
+++ app/components/Contact.jsx
@@ -0,0 +1,87 @@
+import { assets } from "@/assets/assets";
+import Image from "next/image";
+import React, { useState } from "react";
+
+const Contact = () => {
+  const [result, setResult] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "ae14b35c-c104-4eee-aa75-3cf8209c6323");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
+
+  return (
+    <div
+      id="contact"
+      className="w-full px-[12%] py-10 scroll-mt-20 
+    bg-[url('/footer-bg-color.png')] bg-no-repeat bg-center bg-[length:90%_auto]"
+    >
+      <h4 className="text-center mb-2 text-lg font-Ovo">Connect with me</h4>
+      <h2 className="text-center text-4xl font-Ovo">Get in touch</h2>
+
+      <p className="text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo">
+        Have a question or feedback? Just fill out the form below - I'd love to
+        hear from you!
+      </p>
+
+      <form onSubmit={onSubmit} className="max-w-2xl mx-auto">
+        <div className="grid grid-cols-auto gap-6 mt-10 mb-8">
+          <input
+            type="text"
+            placeholder="Enter your name"
+            name="name"
+            required
+            className="flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white"
+          />
+          <input
+            type="email"
+            placeholder="Enter your email"
+            name="email"
+            required
+            className="flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white"
+          />
+        </div>
+        <textarea
+          rows="6"
+          placeholder="Enter your message"
+          name="message"
+          required
+          className="w-full p-4 outline-none border-[0.5px] border-gray-400 rounded-md bg-white mb-6"
+        ></textarea>
+        <button
+          type="submit"
+          className="py-3 px-8 w-max flex items-center justify-between gap-2 bg-black/80 
+          text-white rounded-full mx-auto hover:bg-black duration-500"
+        >
+          Submit now{" "}
+          <Image
+            src={assets.right_arrow_white}
+            alt="arrow icon"
+            className="w-4"
+          />
+        </button>
+        <p className="mt-4 text-center text-gray-500 font-Ovo">{result}</p>
+      </form>
+    </div>
+  );
+};
+
+export default Contact;
--- app/page.js
+++ app/page.js
@@ -5,6 +5,7 @@ import Header from './components/Header';
 import About from './components/About';
 import Services from './components/Services';
 import Work from './components/Work';
+import Contact from './components/Contact';
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
       <About />
       <Services />
       <Work />
+      <Contact />
     </>
   );
 }
