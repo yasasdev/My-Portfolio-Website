e2056a71aedb6ed1c29b09a869c44a39864f6955
yasasdev <yasaslekamge@gmail.com>
2025-10-22 16:23:40 +0530

    Add motion animations to Work component for enhanced user engagement

--- app/components/Work.jsx
+++ app/components/Work.jsx
@@ -1,22 +1,55 @@
 import { assets, workData } from "@/assets/assets";
 import Image from "next/image";
 import React from "react";
+import { motion } from "motion/react";
 
 const Work = ({ isDarkMode }) => {
   return (
-    <div id="work" className="w-full px-[12%] py-10 scroll-mt-20">
-      <h4 className="text-center mb-2 text-lg font-Ovo">My Portfolio</h4>
-      <h2 className="text-center text-4xl font-Ovo">My Latest Work</h2>
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+      id="work"
+      className="w-full px-[12%] py-10 scroll-mt-20"
+    >
+      <motion.h4
+        initial={{ y: -20, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ delay: 0.5, duration: 0.5 }}
+        className="text-center mb-2 text-lg font-Ovo"
+      >
+        My Portfolio
+      </motion.h4>
+      <motion.h2
+        initial={{ y: -20, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ delay: 0.5, duration: 0.5 }}
+        className="text-center text-4xl font-Ovo"
+      >
+        My Latest Work
+      </motion.h2>
 
-      <p className="text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo">
+      <motion.p
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ delay: 0.7, duration: 0.5 }}
+        className="text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo"
+      >
         Welcome to my portfolio! Explore my latest web development projects
         showcasing my skills in creating dynamic, user-friendly, and visually
         appealing applications.
-      </p>
+      </motion.p>
 
-      <div className="grid grid-cols-auto my-10 gap-5 dark:text-black">
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ delay: 0.9, duration: 0.6 }}
+        className="grid grid-cols-auto my-10 gap-5 dark:text-black"
+      >
         {workData.map((project, index) => (
-          <div
+          <motion.div
+            whileHover={{ scale: 1.05 }}
+            transition={{ duration: 0.3 }}
             key={index}
             className="aspect-square bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group"
             style={{ backgroundImage: `url(${project.bgImage})` }}
@@ -36,15 +69,28 @@ const Work = ({ isDarkMode }) => {
                 <Image src={assets.send_icon} alt="Send Icon" className="w-5" />
               </div>
             </div>
-          </div>
+          </motion.div>
         ))}
-      </div>
-      <a href="" className="w-max flex items-center justify-center gap-2 text-gray-700 border-[0.5px]
+      </motion.div>
+      <motion.a
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ delay: 1.1, duration: 0.5 }}
+        href=""
+        className="w-max flex items-center justify-center gap-2 text-gray-700 border-[0.5px]
       border-gray-700 rounded-full py-3 px-10 mx-auto my-20 hover:bg-lightHover duration-500
-      dark:text-white dark:border-white dark:hover:bg-darkHover">
-        Show More <Image src={isDarkMode ? assets.right_arrow_bold_dark : assets.right_arrow_bold} alt="Right Arrow" className="w-4" />
-      </a>
-    </div>
+      dark:text-white dark:border-white dark:hover:bg-darkHover"
+      >
+        Show More
+        <Image
+          src={
+            isDarkMode ? assets.right_arrow_bold_dark : assets.right_arrow_bold
+          }
+          alt="Right Arrow"
+          className="w-4"
+        />
+      </motion.a>
+    </motion.div>
   );
 };
 
