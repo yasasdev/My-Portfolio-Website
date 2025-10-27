de10528d421320ec04ebc199257b086728f750c1
yasasdev <yasaslekamge@gmail.com>
2025-10-27 19:12:56 +0530

    Remove unused "Read more" link in Services component for cleaner code.

--- app/components/Services.jsx
+++ app/components/Services.jsx
@@ -41,14 +41,14 @@ const Services = () => {
             <p className="text-sm text-gray-600 leading-5 dark:text-white/80">
               {description}
             </p>
-            <a href={link} className="flex items-center gap-2 text-sm mt-5">
-              Read more{" "}
+            {/* <a href={link} className="flex items-center gap-2 text-sm mt-5">
+              Read more
               <Image
                 src={assets.right_arrow}
                 alt="right arrow"
                 className="w-4"
               />
-            </a>
+            </a> */}
           </div>
         ))}
       </div>
